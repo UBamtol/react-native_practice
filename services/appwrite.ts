a4d96fa0dd469d6bc2c7cd2fe5f3c855024ab677
UBamtol <yij0325@gmail.com>
2025-08-05 17:14:51 +0900

    feat: appwrite dependency 추가 및 오타 수정

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,18 +1,19 @@
 // track the searches made by a user
+import { Client, Databases, ID, Query } from 'react-native-appwrite';
 
 const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
 const client = new Client()
-  .setEncpoint('https://cloud.appwrite.io/v1')
+  .setEndpoint('https://cloud.appwrite.io/v1')
   .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
 
-const database = new Database(client);
+const database = new Databases(client);
 
 export const updateSearchCount = async (query: string, movie: Movie) => {
   try {
     const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
-      Query.equl('searchTerm', query),
+      Query.equal('searchTerm', query),
     ]);
 
     if (result.documents.length > 0) {
